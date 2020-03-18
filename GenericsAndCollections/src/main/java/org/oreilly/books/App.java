ebb4f0f593c277bb4bc532679399bb034f31b373
Ravindra <ravindra.ranwala@pearson.com>
2020-03-18 20:55:33 +0530

    initial mvn project was added.

---
+++ GenericsAndCollections/src/main/java/org/oreilly/books/App.java
@@ -0,0 +1,13 @@
+package org.oreilly.books;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
