467df0d8821e1f57025b69c3d6d30b06b5fcd8c6
Ravindra <ravindra.ranwala@pearson.com>
2020-05-01 10:14:51 +0530

    A non thread safe stack implementation added.

---
+++ GenericsAndCollections/src/main/java/org/oreilly/books/chapter11/ArrayStack.java
@@ -0,0 +1,38 @@
+package org.oreilly.books.chapter11;
+
+// Example 11-2. A non-thread-safe stack implementation
+public class ArrayStack implements Stack {
+	private final int MAX_ELEMENTS = 10;
+	private int[] stack;
+	private int index;
+
+	public ArrayStack() {
+		stack = new int[MAX_ELEMENTS];
+		index = -1;
+	}
+
+	@Override
+	public void push(int elt) {
+		if (index != stack.length - 1) {
+			index++; // 1
+			stack[index] = elt; // 2
+		} else {
+			throw new IllegalStateException("stack overflow");
+		}
+	}
+
+	@Override
+	public int pop() {
+		if (index != -1) {
+			return stack[index--];
+		} else {
+			throw new IllegalStateException("stack underflow");
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return index == -1;
+	}
+
+}
---
+++ GenericsAndCollections/src/main/java/org/oreilly/books/chapter11/Stack.java
@@ -0,0 +1,9 @@
+package org.oreilly.books.chapter11;
+
+public interface Stack {
+	void push(int elt);
+
+	int pop();
+
+	boolean isEmpty();
+}
