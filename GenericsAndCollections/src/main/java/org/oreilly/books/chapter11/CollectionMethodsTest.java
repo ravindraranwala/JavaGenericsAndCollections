8a959e58280c3167e12a82716cf36b1221ac9897
Ravindra <ravindra.ranwala@pearson.com>
2020-05-03 09:17:21 +0530

    Collection methods and merge task completed.

---
+++ GenericsAndCollections/src/main/java/org/oreilly/books/chapter11/CollectionMethodsTest.java
@@ -0,0 +1,58 @@
+package org.oreilly.books.chapter11;
+
+import static org.oreilly.books.chapter11.StaticTestData.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionMethodsTest {
+	public static void main(String[] args) {
+		mondayTasks.add(new PhoneTask("Ruth", "567 1234"));
+		assert mondayTasks.toString().equals("[code logic, phone Mike, phone Ruth]");
+		Collection<Task> allTasks = new ArrayList<Task>(mondayTasks);
+		allTasks.addAll(tuesdayTasks);
+		assert allTasks.toString().equals("[code logic, phone Mike, phone Ruth, code db, code gui, phone Paul]");
+		
+		boolean wasPresent = mondayTasks.remove(mikePhone);
+		assert wasPresent;
+		assert mondayTasks.toString().equals("[code logic, phone Ruth]");
+		mondayTasks.clear();
+		assert mondayTasks.toString().equals("[]");
+		
+		Collection<Task> tuesdayNonphoneTasks = new ArrayList<Task>(tuesdayTasks);
+		tuesdayNonphoneTasks.removeAll(phoneTasks);
+		assert tuesdayNonphoneTasks.toString().equals("[code db, code gui]");
+		Collection<Task> phoneTuesdayTasks = new ArrayList<Task>(tuesdayTasks);
+		phoneTuesdayTasks.retainAll(phoneTasks);
+		assert phoneTuesdayTasks.toString().equals("[phone Paul]");
+		Collection<PhoneTask> tuesdayPhoneTasks = new ArrayList<PhoneTask>(phoneTasks);
+		tuesdayPhoneTasks.retainAll(tuesdayTasks);
+		assert tuesdayPhoneTasks.toString().equals("[phone Paul]");
+		
+		assert tuesdayPhoneTasks.contains(paulPhone);
+		assert tuesdayTasks.containsAll(tuesdayPhoneTasks);
+		assert mondayTasks.isEmpty();
+		assert mondayTasks.size() == 0;
+		
+		// throws ConcurrentModificationException
+		for (Task t : tuesdayTasks) {
+			if (t instanceof PhoneTask) {
+				tuesdayTasks.remove(t);
+			}
+		}
+		// throws ConcurrentModificationException
+		for (Iterator<Task> it = tuesdayTasks.iterator(); it.hasNext();) {
+			Task t = it.next();
+			if (t instanceof PhoneTask) {
+				tuesdayTasks.remove(t);
+			}
+		}
+		for (Iterator<Task> it = tuesdayTasks.iterator(); it.hasNext();) {
+			Task t = it.next();
+			if (t instanceof PhoneTask) {
+				it.remove();
+			}
+		}
+	}
+}
