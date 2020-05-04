7daa631fd94004f1f1bf186f27635777dabe8a03
Ravindra <ravindra.ranwala@pearson.com>
2020-05-04 11:24:32 +0530

    A simple use of linked hash set added.

---
+++ GenericsAndCollections/src/main/java/org/oreilly/books/chapter12/LinkedHashSetTest.java
@@ -0,0 +1,14 @@
+package org.oreilly.books.chapter12;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetTest {
+	public static void main(String[] args) {
+		Set<Character> s2 = new LinkedHashSet<Character>(8);
+		Collections.addAll(s2, 'a', 'b', 'j');
+		// iterators of a LinkedHashSet return their elements in proper order:
+		assert s2.toString().equals("[a, b, j]");
+	}
+}
