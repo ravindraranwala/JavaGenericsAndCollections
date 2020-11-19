929c027eff270b86bc741ca4df9b599711d149c0
Ravindra <ravindra.ranwala@pearson.com>
2020-11-19 08:00:33 +0530

    inline comments were added.

--- GenericsAndCollections/src/main/java/org/oreilly/books/chapter6/IndecentExposure.java
+++ GenericsAndCollections/src/main/java/org/oreilly/books/chapter6/IndecentExposure.java
@@ -8,12 +8,16 @@ public class IndecentExposure {
 	public static void main(String[] args) {
 		Integer[] ints = new Integer[] { 1 };
 		Number[] nums = ints;
+		// The actual error is here, and the exception is thrown in exactly the same
+		// line.
 		// nums[0] = 1.01; // array store exception
 		int n = ints[0];
 
 		List<Integer>[] intLists = (List<Integer>[]) new List[] { Arrays.asList(1) }; // unchecked cast
 		List<? extends Number>[] numLists = intLists;
+		// The actual error is here.
 		numLists[0] = Arrays.asList(1.01);
+		// But the exception is thrown else where in the code. This is confusing.
 		int n2 = intLists[0].get(0); // class cast exception
 	}
 
