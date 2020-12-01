5885155a1f5138701c2be787101289c7aa679d03
Ravindra <ravindra.ranwala@pearson.com>
2020-12-01 10:59:08 +0530

    visitor pattern code refactored.

--- GenericsAndCollections/src/main/java/org/oreilly/books/chapter9/visitor/Tree.java
+++ GenericsAndCollections/src/main/java/org/oreilly/books/chapter9/visitor/Tree.java
@@ -1,7 +1,7 @@
 package org.oreilly.books.chapter9.visitor;
 
 public abstract class Tree<E> {
-	public interface Visitor<E, R> {
+	public static interface Visitor<E, R> {
 		public R leaf(E elt);
 
 		public R branch(R left, R right);
@@ -18,6 +18,7 @@ public abstract class Tree<E> {
 	}
 
 	public static <T> Tree<T> branch(final Tree<T> l, final Tree<T> r) {
+		// This is our composite element.
 		return new Tree<T>() {
 			public <R> R visit(Visitor<T, R> v) {
 				return v.branch(l.visit(v), r.visit(v));
